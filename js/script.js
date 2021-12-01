aeb566c6fa3c96e3d27fcfdc8ed595b9ec87e787
Dandav2923 <daniele.emili23@gmail.com>
2021-12-01 18:05:47 +0100

    creazione griglie

--- js/script.js
+++ js/script.js
@@ -11,38 +11,43 @@ containerHeader.append(difficulty);
 
 
 // far vedere la griglia al click della option
-let easy = document.getElementById('easy'); 
-console.log(easy);
-easy.addEventListener('click', function() {   
+let button = document.getElementById('button'); 
+// console.log(button);
+button.addEventListener('click', function() {   
     // creazione della griglia 10x10 
-    let containerMain = document.querySelector('.col-6');
-    // containerMain.innerHTML = '';
-    for (let counter = 1; counter <= 100; counter++) {
-        // console.log(counter);
-        const element = document.createElement('div');
-        element.classList.add('box');
-        element.innerHTML = counter;
-        containerMain.append(element);
+    let containerMain = document.querySelector('.grid-container');
+    let select = document.getElementById('select');
+    let value = select.value;
+    if (value == 'easy'){
+        containerMain.innerHTML = '';
+        for (let counter = 1; counter <= 100; counter++) {
+            // console.log(counter);
+            const element = document.createElement('div');
+            element.classList.add('box');
+            element.innerHTML = counter;
+            containerMain.append(element);
+        }
     }
+    else if (value == 'hard') {
+        containerMain.innerHTML = '';
+        for (let counter = 1; counter <= 81; counter++) {
+            // console.log(counter);
+            const element = document.createElement('div');
+            element.classList.add('box');
+            element.innerHTML = counter;
+            containerMain.append(element);
+        }
+        
+    }
+    else {
+        containerMain.innerHTML = '';
+        for (let counter = 1; counter <= 49; counter++) {
+            // console.log(counter);
+            const element = document.createElement('div');
+            element.classList.add('box');
+            element.innerHTML = counter;
+            containerMain.append(element);
+        }
+    }
+    
 });
-
-
-// // creazione della griglia 9x9
-// let containerMain = document.querySelector('.col-6');
-// for (let counter = 1; counter <= 81; counter++) {
-//     // console.log(counter);
-//     const element = document.createElement('div');
-//     element.classList.add('box');
-//     element.innerHTML = counter;
-//     containerMain.append(element);
-// }
-
-// // creazione della griglia 7x7
-// let containerMain = document.querySelector('.col-6');
-// for (let counter = 1; counter <= 49; counter++) {
-//     // console.log(counter);
-//     const element = document.createElement('div');
-//     element.classList.add('box');
-//     element.innerHTML = counter;
-//     containerMain.append(element);
-// }
