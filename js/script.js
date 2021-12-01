d2d70016a77e6a5cef8a97c222475328fed05293
Dandav2923 <daniele.emili23@gmail.com>
2021-12-01 15:59:17 +0100

    primo elemento della header

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 // alert ('ciao');
+// creare gli elementi da inserire nel dom della sezione header 
+let containerHeader = document.querySelector('.col-3');
+console.log(containerHeader);
+let difficulty = document.createElement('span');
+const string = 'Difficoltà:';
+difficulty.append(string);
+containerHeader.append(difficulty);
+
